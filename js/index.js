403eab117d7985d55f4d42a195b78fcccbb98597
Irma Blanco <irblancofdz@gmail.com>
2020-05-27 21:18:49 +0200

    Iteration one

--- js/index.js
+++ js/index.js
@@ -3,5 +3,11 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext('2d'); 
+    const carImage = new Image();
+    carImage.src = '../images/road.png';
+    ctx.drawImage(carImage, 50, 50, 200, 200);
+  }
 };
